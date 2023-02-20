673c23ac7bc2a0bf117a022166b7e8c7385d4a67
Lino Steinhau <lis@entimo.de>
2023-02-20 17:45:47 +0100

    update

--- bn_classifier/classifier.py
+++ bn_classifier/classifier.py
@@ -1,3 +1,4 @@
+from typing import Union
 import torch
 import torch.nn as nn
 from importlib_resources import files
@@ -6,7 +7,7 @@ import torchvision.transforms as transforms
 import os
 import numpy as np
 
-class _CellModel(nn.Module):
+class CellModel(nn.Module):
 
     def __init__(self):
         super().__init__()
@@ -36,14 +37,16 @@ class _CellModel(nn.Module):
         x = torch.flatten(x, 3)  # flatten all dimensions except batch
         return x
 
-PATH_LOAD = files('bn_classifier').joinpath('weights/best-augment.pth')
-net = _CellModel()
-net.load_state_dict(torch.load(PATH_LOAD, map_location=torch.device('cpu')))
-net.double()
 
-_best_thresholds = torch.Tensor([0.37, 0.29, 0.62, 0.66, 0.29, 0.55])
-_radius = 5
-_binary_classes_readable = [
+class BNClassifier:
+    PATH_LOAD = files('bn_classifier').joinpath('weights/best-augment.pth')
+    net = CellModel()
+    net.load_state_dict(torch.load(PATH_LOAD, map_location=torch.device('cpu')))
+    net.double()
+
+    _best_thresholds = torch.Tensor([0.37, 0.29, 0.62, 0.66, 0.29, 0.55])
+    _radius = 5
+    binary_classes_readable = [
         "empty",
         "egg",
         "larva",
@@ -51,93 +54,127 @@ _binary_classes_readable = [
         "old pupa",
         "bee head"
     ]
-_scale_factor = 64/400
-class BNClassifier:
-    
+    _scale_factor = 64/400
+
     @staticmethod
-    def feed_image(img_path: str):
+    def get_trained_model() -> nn.Module:
+        """Return the model with the trained weights applied
+
+        Returns:
+            nn.Module: The model
+        """
+        return BNClassifier.net
+
+    @staticmethod
+    def get_untrained_model() -> nn.Module:
+        """Return the model with the default weights
+
+        Returns:
+            nn.Module: The model
+        """
+        return CellModel()
+
+    @staticmethod
+    def feed_image(image: Union[str, torch.Tensor]) -> torch.Tensor:
         """Feed one image to the network and return the raw output
 
         Args:
-            img_path (str): The path to the image
+            image (str|Tensor): The path to the image or the image tensor
 
         Returns:
             torch.Tensor: The prediction of the network
         """
-        image = torch.Tensor(io.imread(img_path)).double()[:, :, :3].permute(2, 0, 1)
-        
-        image = transforms.functional.resize(image, (int(image.shape[1]*_scale_factor),int(image.shape[2]*_scale_factor)))
+        if type(image) == str:
+            image = torch.Tensor(io.imread(image)).double()[:, :, :3].permute(2, 0, 1)
+        elif type(image) == torch.Tensor:
+            assert len(image.shape) == 3
+            if image.shape[2] == 3 or image.shape[2] == 4:
+                image = image.permute(2, 0, 1)
+            assert image.shape[0] == 3 or image.shape[0] == 4
+            image = image.double()[:, :, :3]
+        else:
+            raise TypeError(f"Image type {type(image)} not supported")
+
+        image = transforms.functional.resize(
+            image, (int(image.shape[1]*BNClassifier._scale_factor), int(image.shape[2]*BNClassifier._scale_factor)))
         image = transforms.Normalize((127.5, 127.5, 127.5), (127.5, 127.5, 127.5))(image)
 
-        pred_scores = net(image.unsqueeze(0)).detach().squeeze(0).permute(1,2,0)
+        pred_scores = BNClassifier.net(image.unsqueeze(0)).detach().squeeze(0).permute(1, 2, 0)
         return pred_scores
-    
+
     @staticmethod
-    def feed_images(img_dir: str):
+    def feed_images_from_dir(img_dir: str) -> dict[str, torch.Tensor]:
         """Feed all images from a directory to the network and return the predictions
 
         Args:
-            img_dir (str): The directory path
+            image (str|Tensor): The path to the image or the image tensor
 
         Returns:
             dict: A dictionary with the filenames as keys and the predictions as values
         """
         out = {}
         for f in os.listdir(img_dir):
-            out[f] = BNClassifier.feed_image(os.path.join(img_dir,f))
+            out[f] = BNClassifier.feed_image(os.path.join(img_dir, f))
         return out
 
     @staticmethod
-    def get_prediction_points(img_path: str):
+    def get_prediction_points(image: Union[str, torch.Tensor], classes_as_strings=False) -> tuple[list[list[float]], list[Union[int, str]], list[str]]:
         """Feed one image to the network and return the prediction points (local maxima filtered)
 
         Args:
-            img_path (str): The path to the image
+            image (str|Tensor): The path to the image or the image tensor
 
         Returns:
-            tuple: Tuple of (points, class_values)
+            tuple: Tuple of (points, classes [as indices or strings], class values)
         """
-        pred_scores = BNClassifier.feed_image(img_path)
-        
-        scores_filtered = torch.zeros((pred_scores.shape[0],pred_scores.shape[1]))
+        pred_scores = BNClassifier.feed_image(image)
+
+        scores_filtered = torch.zeros((pred_scores.shape[0], pred_scores.shape[1]))
         score_classes = []
         for y in range(pred_scores.shape[0]):
-                score_classes.append([])
-                for x in range(pred_scores.shape[1]):
-                    if len(pred_scores[y,x][pred_scores[y,x] >= _best_thresholds]) > 0:
-                        scores_filtered[y,x] = pred_scores[y,x][pred_scores[y,x] >= _best_thresholds].mean()
-                        score_classes[y].append(pred_scores[y,x])
-                    else:
-                        scores_filtered[y,x] = 0
-                        score_classes[y].append(torch.zeros(len(pred_scores[y,x])))
-        
+            score_classes.append([])
+            for x in range(pred_scores.shape[1]):
+                if len(pred_scores[y, x][pred_scores[y, x] >= BNClassifier._best_thresholds]) > 0:
+                    scores_filtered[y, x] = pred_scores[y, x][pred_scores[y, x] >= BNClassifier._best_thresholds].mean()
+                    score_classes[y].append(pred_scores[y, x])
+                else:
+                    scores_filtered[y, x] = 0
+                    score_classes[y].append(torch.zeros(len(pred_scores[y, x])))
+
         scores_copy = scores_filtered.clone()
         for y in range(scores_filtered.shape[0]):
             for x in range(scores_filtered.shape[1]):
-                scores_copy[y:y+_radius+1,x:x+_radius+1] = torch.max(scores_copy[y:y+_radius+1,x:x+_radius+1], scores_filtered[y:y+_radius+1,x:x+_radius+1].max())
-        score_classes = [[BNClassifier.multi_score_to_readable(t, _best_thresholds.numpy()) for t in l] for l in score_classes]
-        
+                scores_copy[y:y+BNClassifier._radius+1, x:x+BNClassifier._radius+1] = torch.max(
+                    scores_copy[y:y+BNClassifier._radius+1, x:x+BNClassifier._radius+1], scores_filtered[y:y+BNClassifier._radius+1, x:x+BNClassifier._radius+1].max())
+        score_classes = [[BNClassifier.multi_score_to_readable(
+            t, BNClassifier._best_thresholds.numpy()) for t in l] for l in score_classes]
+
         scores_copy[scores_copy != scores_filtered] = 0
-        
+
         org_pad = 0
         pad = 31
         scale = 4
         point_pad = 0
         pred_points = []
+        pred_class_indices = []
         pred_classes = []
-        # scores_copy[scores_copy < best_thresholds[i]] = 0
+        class_values = []
         for y in range(point_pad, scores_copy.shape[0]-point_pad):
             for x in range(point_pad, scores_copy.shape[1]-point_pad):
-                if scores_copy[y,x] > 0:
-                    pred_points.append(((1/_scale_factor)*(scale*x+pad)+org_pad,(1/_scale_factor)*(scale*y+pad)+org_pad))
+                if scores_copy[y, x] > 0:
+                    pred_points.append(((1/BNClassifier._scale_factor)*(scale*x+pad)+org_pad,
+                                       (1/BNClassifier._scale_factor)*(scale*y+pad)+org_pad))
+                    if score_classes[y][x] not in class_values:
+                        class_values.append(score_classes[y][x])
+                    pred_class_indices.append(class_values.index(score_classes[y][x]))
                     pred_classes.append(score_classes[y][x])
         pred_points = np.array(pred_points)
-        return pred_points, pred_classes
+        return pred_points, pred_classes if classes_as_strings else pred_class_indices, class_values
 
     @staticmethod
-    def multi_score_to_readable(multi_score, thresholds=np.array([0.5] * len(_binary_classes_readable))):
+    def multi_score_to_readable(multi_score: Union[np.ndarray, torch.Tensor], thresholds=np.array([0.5] * len(binary_classes_readable))) -> str:
         """Convert a multi-label score to a readable string
-        
+
         Args:
             multi_score (array-like): The multi-label score (length 6)
 
@@ -148,8 +185,9 @@ class BNClassifier:
             multi_score = multi_score.cpu().detach().numpy()
         if (multi_score - thresholds).max() < 0:
             return "(unknown)"
-        if np.argmax(multi_score) == _binary_classes_readable.index("empty"):
+        if np.argmax(multi_score) == BNClassifier.binary_classes_readable.index("empty"):
             return "(empty)"
         return (
-            "(" + ", ".join([_binary_classes_readable[i] for i, x in enumerate(multi_score) if x-thresholds[i] >= 0 and i!=_binary_classes_readable.index("empty")]) + ")"
+            "(" + ", ".join([BNClassifier.binary_classes_readable[i] for i, x in enumerate(multi_score)
+                             if x-thresholds[i] >= 0 and i != BNClassifier.binary_classes_readable.index("empty")]) + ")"
         )
